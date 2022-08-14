e8fa53fc32de4ff3b65e720718ad8f2c222b506d
sparsh-kamat <kamatsparsh@gmail.com>
2022-08-14 12:01:54 +0530

    add

---
+++ Week 1/input.py
@@ -0,0 +1,2 @@
+n = input("What is your name?")
+print("Hello", n)
---
+++ Week 2/assignment3.py
@@ -0,0 +1,7 @@
+s = input()
+a = int(input())
+for i in range(a):
+    if i == a - 1:
+        print(s, end='')
+    else:
+        print(s)
